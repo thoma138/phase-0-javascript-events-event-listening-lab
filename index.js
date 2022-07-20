25c9b374d8fb5085e0679c7117361e1adf0b99a6
thoma138 <thomaselliott@live.com>
2022-07-20 11:16:13 +1000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-let input = document.getElementById("clicker");
-
 function addingEventListener() {
-  input.addEventListener("click", function () {
-    alert("I was clicked");
-  });
+  const input = document.getElementById("input");
+
+  function clickAlert() {
+    alert("I was clicked!");
+  }
+
+  input.addEventListener("click", clickAlert);
 }
+
 addingEventListener();
